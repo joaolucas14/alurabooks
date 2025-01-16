0ec0580663d789eba5199760ef590861ee4b7024
joaolucas14 <jlparceiro10@gmail.com>
2025-01-16 11:39:35 -0300

    adiciona interface IPedido e implementa listagem dinâmica de pedidos

--- src/paginas/Pedidos/index.tsx
+++ src/paginas/Pedidos/index.tsx
@@ -1,27 +1,50 @@
 import { AbBotao } from "ds-alurabooks";
+import axios from "axios";
 import "./Pedidos.css";
+import { useEffect, useState } from "react";
+import { IPedido } from "../../interfaces/Ipedido";
 
 export default function Pedidos() {
+  const [pedidos, setPedidos] = useState<IPedido[]>([]);
+  const formatador = Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+    axios
+      .get<IPedido[]>("http://localhost:8000/pedidos", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((resposta) => setPedidos(resposta.data))
+      .catch((erro) => console.log(erro));
+  }, []);
   return (
     <section className="pedidos">
       <h1>Meus Pedidos</h1>
-      <div className="pedido">
-        <ul>
-          <li>
-            Pedido: <strong>123456</strong>
-          </li>
-          <li>
-            Data: <strong>15/01/2025</strong>
-          </li>
-          <li>
-            Valor total: <strong>R$48</strong>
-          </li>
-          <li>
-            Entrega realizada em: <strong>17/01/2025</strong>
-          </li>
-        </ul>
-        <AbBotao texto="Detalhes" />
-      </div>
+      {pedidos.map((pedido) => (
+        <div className="pedido" key={pedido.id}>
+          <ul>
+            <li>
+              Pedido: <strong>{pedido.id}</strong>
+            </li>
+            <li>
+              Data:{" "}
+              <strong>{new Date(pedido.data).toLocaleDateString()}</strong>
+            </li>
+            <li>
+              Valor total: <strong>{formatador.format(pedido.total)}</strong>
+            </li>
+            <li>
+              Entrega realizada em:{" "}
+              <strong>{new Date(pedido.entrega).toLocaleDateString()}</strong>
+            </li>
+          </ul>
+          <AbBotao texto="Detalhes" />
+        </div>
+      ))}
     </section>
   );
 }
