25f1dcb0228209be46271dc934717d4ed12fd44f
joaolucas14 <jlparceiro10@gmail.com>
2025-04-04 10:54:45 -0300

    adiciona componente Loader e TituloPrincipal, implementa página de Categoria com carregamento dinâmico

---
+++ src/componentes/Loader/index.tsx
@@ -0,0 +1,11 @@
+import "./Loader.css";
+export default function Loader() {
+  return (
+    <div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
