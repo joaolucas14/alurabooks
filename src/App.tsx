7dee2386f90d425f7c230bc8ed0471cb5b803799
joaolucas14 <jlparceiro10@gmail.com>
2025-04-05 11:40:11 -0300

    fazendo a primeira query

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Rotas from "./rotas";
+import ABApolloCliente from "./componentes/ABApolloCliente";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Rotas />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <ABApolloCliente>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Rotas />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ABApolloCliente>
   );
 }
 
--- src/componentes/ListaLivros/index.tsx
+++ src/componentes/ListaLivros/index.tsx
@@ -1,22 +1,38 @@
-import { useQuery } from "@tanstack/react-query";
+import { gql, useQuery } from "@apollo/client";
 import { ICategoria } from "../../interfaces/ICategoria";
-import { obterProdutosDaCategoria } from "../../http";
 import CardLivro from "../CardLivro";
 import "./ListaLivros.css";
+import { ILivro } from "../../interfaces/ILivro";
 
 interface ListaLivrosProps {
   categoria: ICategoria;
 }
 
+const OBTER_LIVROS = gql`
+  # Write your query or mutation here
+  query ObterLivros {
+    livros {
+      id
+      slug
+      titulo
+      imagemCapa
+      opcoesCompra {
+        id
+        preco
+      }
+    }
+  }
+`;
+
 export default function ListaLivros({ categoria }: ListaLivrosProps) {
-  const { data: produtos } = useQuery({
-    queryKey: ["buscaLivrosPorCategoria", categoria],
-    queryFn: () => obterProdutosDaCategoria(categoria),
-  });
-  console.log(produtos);
+  const { data } = useQuery<{ livros: ILivro[] }>(OBTER_LIVROS);
+  // const { data: produtos } = useQuery({
+  //   queryKey: ["buscaLivrosPorCategoria", categoria],
+  //   queryFn: () => obterProdutosDaCategoria(categoria),
+  // });
   return (
     <section className="livros">
-      {produtos?.map((livro) => (
+      {data?.livros.map((livro) => (
         <CardLivro livro={livro} key={livro.id} />
       ))}
     </section>
